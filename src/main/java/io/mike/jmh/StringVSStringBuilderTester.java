e7cc2c5530247d0af640ec9fe1900aa57ebbb343
zhaoming-mike <zhaoming23@github.com>
2017-08-15 14:31:52 +0800

    去掉公司的版权信息头

--- src/main/java/io/mike/jmh/StringVSStringBuilderTester.java
+++ src/main/java/io/mike/jmh/StringVSStringBuilderTester.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2017 YY Inc.
- * All Rights Reserved.
- * This program is the confidential and proprietary information of 
- * YY Inc. ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance with
- * the terms of the license agreement you entered into with YY Inc.
- */
 package io.mike.jmh;
 
 import java.util.concurrent.TimeUnit;
