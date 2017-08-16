5bbbb11691f506f8401b7f281d0cd02b1edc91a7
Mike <zhaoming23@github.com>
2017-08-16 17:12:27 +0800

    添加单元测试

--- src/main/java/io/mike/http/server/SimpleHttpServer.java
+++ src/main/java/io/mike/http/server/SimpleHttpServer.java
@@ -2,39 +2,108 @@ package io.mike.http.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.net.httpserver.*;
 
 import io.mike.http.server.handler.HelloHttpHandler;
 
+@SuppressWarnings("restriction")
 public class SimpleHttpServer {
 
-	@SuppressWarnings("restriction")
-	public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(SimpleHttpServer.class);
+	
+	static volatile HttpServer httpServer = null;
+	static final int STOP_DELAY_DEFAULT_SECOND = 3;
+	
+	public static void startServer() {
+		main(null);
+	}
+	public static void startServer(int port, String path) {
+		if(httpServer != null) {
+			log.error("http server is already started.");
+		}
 		try {
-			String path = "/hello";
-			int port = 8888;
-			if(args.length == 2) {
-				try {
-					path = "/" + args[0];
-					port = Integer.parseInt(args[1]);
-				} catch (Exception e) {
-					//eat it.
-				}
-			}
-			HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+			httpServer = HttpServer.create(new InetSocketAddress(port), 0);
 			httpServer.createContext(path, new HelloHttpHandler());
 			httpServer.setExecutor(null);
 			httpServer.start();
-			System.out.println("http server started on port:" + port);
-			System.out.println("http server default path:" + path);
+			printLines();
+			log.info("http server started on port:" + port);
+			log.info("http server path:" + path);
+			beforeShutdown();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static void printLines() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\r\n");
+		sb.append("\r\n");
+		for (int i = 0; i < 100; i++) {
+			sb.append("=");
+		}
+		log.info(sb.toString());
+	}
+
+	private static void beforeShutdown() {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				SimpleHttpServer.stopServer();
+			}
+			
+		});
+	}
+	
+	public static void stopServer() {
+		if(httpServer == null) {
+			log.info("no http server need to stop.");
+		}
+		
+		printCountDownInfo();
+		
+		httpServer.stop(STOP_DELAY_DEFAULT_SECOND);
+		log.info("http server is stopped.");
+	}
+	
+	private static void printCountDownInfo() {
+		new Thread() {
+			@Override
+			public void run() {
+				for (int i = STOP_DELAY_DEFAULT_SECOND; i > 0 ; i--) {
+					log.info(String.format("http server will stop after %s seconds.", i));
+					try {
+						TimeUnit.SECONDS.sleep(1);
+					} catch (InterruptedException e) {
+						log.error(e.getMessage(), e);
+					}
+				}
+			}
+		}.start();
+	}
+	
+	public static void main(String[] args) {
+		String path = "/hello";
+		int port = 8888;
+		if(args.length == 2) {
+			try {
+				path = "/" + args[0];
+				port = Integer.parseInt(args[1]);
+			} catch (Exception e) {
+				//eat it.
+			}
+		}
+		startServer(port, path);
 	}
 
 	public int times(int n) {
 		return n * 10;
 	}
 
+	
 }
---
+++ src/test/java/io/mike/http/server/SimpleHttpServerTester.java
@@ -0,0 +1,88 @@
+package io.mike.http.server;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhaoming
+ *
+ * 2017年8月16日 下午3:38:18
+ */
+public class SimpleHttpServerTester {
+
+	private static final Logger log = LoggerFactory.getLogger(SimpleHttpServerTester.class);
+	
+	public static final int DEFAULT_PORT = 8888;
+	public static final String DEFAULT_PATH = "/hello";
+	
+	@BeforeClass
+	public static void beforeClass() throws InterruptedException {
+		
+		log.debug("start http server for test.");
+		SimpleHttpServer.startServer(
+				DEFAULT_PORT, 
+				DEFAULT_PATH);
+		//TimeUnit.SECONDS.sleep(3);
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		log.debug("test finished stop http server.");
+		SimpleHttpServer.stopServer();
+	}
+	
+	@Before
+	public void before() {
+		log.debug("before each test...");
+	}
+	
+	@After
+	public void after() {
+		log.debug("after each test...");
+	}
+	
+	@Test
+	public void test_httpclient() {
+		log.debug("test http server response by httpclient.");
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		String uri = String.format("http://localhost:%s%s?", new Object[]{DEFAULT_PORT,DEFAULT_PATH});
+		log.debug("uri:" + uri);
+		HttpGet httpGet = new HttpGet(uri);
+		CloseableHttpResponse response = null;
+		try {
+			response = httpclient.execute(httpGet);
+		    //<...>
+			log.debug("response status:" + response.getStatusLine());
+		} catch (ClientProtocolException e) {
+			log.error(e.getMessage(), e);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		} finally {
+		    try {
+				response.close();
+			} catch (IOException e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+	}
+	
+	@Test
+	public void test2() {
+		log.debug("test2");
+	}
+
+	
+}
