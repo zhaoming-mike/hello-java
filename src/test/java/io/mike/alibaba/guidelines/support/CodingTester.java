31ef3bd6954632588bc2c90d38c11df66257d0dd
Mike <zhaoming23@github.com>
2017-09-26 12:38:21 +0800

    编程规范添加

--- src/test/java/io/mike/alibaba/guidelines/support/CodingTester.java
+++ src/test/java/io/mike/alibaba/guidelines/support/CodingTester.java
@@ -1,11 +1,9 @@
 package io.mike.alibaba.guidelines.support;
-import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * 1、编程规范
@@ -54,6 +52,105 @@ public class CodingTester {
 		assertThat(pingfenByName, 	is("不太好的拼音和英文混合命名【评分】"));
 		assertThat(一个变量, 			is("不太好的中文命名"));
 	}
+	
+	@Test
+	@Level(LevelType.enforce)
+	public void test_name_upper_camel_case() throws Exception {
+		//【强制】类名使用 UpperCamelCase 风格，必须遵从驼峰形式，但以下情形例外：DO / BO / DTO / VO / AO
+		
+		//分层领域模型规约
+		/**
+		 *  Plain Ordinary Java Object
+			POJO {
+				DO: 	数据对象 		Data Object
+				DTO:	数据传输对象	Data Transfer Object
+				BO:		业务对象		Business Object
+				VO:		展示对象		View Object
+			}
+		 */
+		
+		// AO:	应用对象(在 Web 层与 Service 层之间抽象的复用对象模型，极为贴近展示层，复用度不高。)	Application Object
+		
+		MarcoPolo marcoPolo = new MarcoPolo("OK的驼峰命名");
+		assertThat(marcoPolo.desc, is("OK的驼峰命名"));
+		
+		UserDO userDO = new UserDO("Mike");
+		assertThat(userDO.name, is("Mike"));
+	}
+	
+	public static class MarcoPolo { private String desc; public MarcoPolo(String desc) { this.desc = desc; } }
+	public static class UserDO { private String name; public UserDO(String name) { this.name = name; } }
+	
+	@Test
+	@Level(LevelType.enforce)
+	public void test_name_lower_camel_case() throws Exception {
+		//【强制】方法名、参数名、成员变量、局部变量都统一使用lowerCamelCase风格，必须遵从驼峰形式。
+		assertThat(localValue(), is("不错的方法命名"));
+	}
+	
+	private String localValue() {
+		return "不错的方法命名";
+	}
+
+	@Test
+	@Level(LevelType.enforce)
+	public void test_name_constant() throws Exception {
+		assertThat(MAX_VALUE, is("不错的常量命名"));
+	}
+	
+	public static final String MAX_VALUE = "不错的常量命名";
+	
+	@Test
+	@Level(LevelType.enforce)
+	public void test_name_abstract_class() throws Exception {
+		AbstractUser abstractUser = new AbstractUser("不错的抽象类命名") {};
+		BaseUser baseUser = new BaseUser("不错的抽象类命名") {};
+		
+		assertThat(abstractUser.desc, is("不错的抽象类命名"));
+		assertThat(baseUser.desc, is("不错的抽象类命名"));
+	}
+	
+	public static abstract class AbstractUser { private String desc; public AbstractUser(String desc) { this.desc = desc; } }
+	public static abstract class BaseUser { private String desc; public BaseUser(String desc) { this.desc = desc; } }
+	
+	@Test
+	@Level(LevelType.enforce)
+	public void test_name_array() throws Exception {
+		String[] args = {"不错的", "数组", "类型声明" };
+		String infos[] = {"不太好的", "数组", "类型声明" };
+		
+		assertThat(args[0], is("不错的"));
+		assertThat(args[1], is("数组"));
+		assertThat(args[2], is("类型声明"));
+		
+		assertThat(infos[0], is("不太好的"));
+		assertThat(infos[1], is("数组"));
+		assertThat(infos[2], is("类型声明"));
+	}
+	
+	@Test
+	@Level(LevelType.enforce)
+	public void test_name_field_name() throws Exception {
+		InfoDO infoDO = new InfoDO(true, "不太好的属性名字:isOk,会被某些反序列化框架误认为是'ok'，导致异常。");
+		assertThat(infoDO.desc, is("不太好的属性名字:isOk,会被某些反序列化框架误认为是'ok'，导致异常。"));
+	}
+	
+	public static class InfoDO { 
+		private boolean isOk; 
+		private String desc; 
+		public InfoDO(boolean isOk, String desc) {
+			this.isOk = isOk;
+			this.desc = desc;
+		}
+		public boolean isOk() {
+			return isOk;
+		}
+		public void setOk(boolean isOk) {
+			this.isOk = isOk;
+		}
+		
+	}
+	
 	@Test
 	public void test() throws Exception {
 		Assert.assertEquals(1, 1);
