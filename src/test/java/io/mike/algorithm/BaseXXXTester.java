e2a431cded8a070ebaefa3416157e19b28c62ca0
Mike <zhaoming23@github.com>
2017-10-24 14:50:08 +0800

    程序员节题目测试【摩尔斯电码】

--- src/test/java/io/mike/algorithm/BaseXXXTester.java
+++ src/test/java/io/mike/algorithm/BaseXXXTester.java
@@ -2,7 +2,11 @@ package io.mike.algorithm;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
+import org.springframework.util.Base64Utils;
 
 import com.google.common.io.BaseEncoding;
 
@@ -16,13 +20,34 @@ public class BaseXXXTester {
 	public void test_base16() throws Exception {
 		BaseEncoding base16 = BaseEncoding.base16();
 		String msg = "68656C6C6F";
+//		String msg = "F963UF91XXF59Y9F6211F6765F4V86";
 		boolean canDecode = base16		.canDecode(msg);
+		System.out.println(canDecode);
 		if(canDecode) {
 			byte[] decode = base16.decode(msg);
 			String m = new String(decode);
+			System.out.println(m);
 			assertThat(m, is("hello"));
 		}
 	}
 	
+	@Test
+	public void test_base64() {
+		System.out.println(Base64Utils.encodeToString("祝你节日快乐".getBytes()));
+		
+	}
+	
+	@Test
+	public void test_1024() {
+		String a1 = "F963U"; 
+		String a2 = "F91XX";
+		String a3 = "F59Y9";
+		String a4 = "F6211";
+		String a5 = "F6765";
+		String a6 = "F4V86";
+		String all = a1 + a2 + a3 + a4 + a5 + a6;
+		System.out.println(all);
+		System.out.println(new String(Base64.decodeBase64(all)));
+	}
 	
 }
