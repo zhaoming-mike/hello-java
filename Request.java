e767697ecdf280408e5d04bcf1289bde73c5c5b4
Mike <zhaoming23@gmail.com>
2018-01-23 13:03:19 +0800

    add *.java at 2018-01-23 13:03:17

--- Request.java
+++ Request.java
@@ -545,4 +545,4 @@ public final class Request {
         return reqProcessorMX.getStage()==org.apache.coyote.Constants.STAGE_SERVICE;
     }
 }
-//edit at 2018-01-23 12:53:17
+//edit at 2018-01-23 13:03:17
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2018-01-23 12:53:17
+//edit at 2018-01-23 13:03:17
