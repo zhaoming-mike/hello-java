f26d910fe97f38b4c5b120c2ba08712c9d4aef1d
Mike <zhaoming23@gmail.com>
2018-04-24 20:40:07 +0800

    add *.java at 2018-04-24 20:40:01

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello { 
 	public void test() { 
-		// edit at 2018-04-24 20:30:00
+		// edit at 2018-04-24 20:40:01
 	}
 } 
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2018-04-24 20:30:00
+//edit at 2018-04-24 20:40:01
