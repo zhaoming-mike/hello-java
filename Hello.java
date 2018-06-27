cff2fa92387242bc868c84598cad9448ed0c2079
Mike <zhaoming23@gmail.com>
2018-06-27 17:15:14 +0800

    add *.java at 2018-06-27 17:15:10

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello { 
 	public void test() { 
-		// edit at 2018-06-27 17:05:17
+		// edit at 2018-06-27 17:15:10
 	}
 } 
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2018-06-27 17:05:17
+//edit at 2018-06-27 17:15:10
