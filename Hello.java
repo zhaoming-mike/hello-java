99502bbc56b3398c3e188e2d1a9122225bc4842e
zhaoming <zhaoming@gacrnd.com>
2019-03-19 20:50:02 +0800

    add *.java at 2019-03-19 20:50:00

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello { 
 	public void test() { 
-		// edit at 2019-03-19 20:40:01
+		// edit at 2019-03-19 20:50:00
 	}
 } 
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2019-03-19 20:40:01
+//edit at 2019-03-19 20:50:00
