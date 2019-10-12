16eacd509c7a0900c72d24b2fd387f4ffa7e5b9a
zhaoming <zhaoming@gacrnd.com>
2019-10-13 01:20:44 +0800

    add *.java at 2019-10-13 01:20:44

--- Request.java
+++ Request.java
@@ -545,4 +545,4 @@ public final class Request {
         return reqProcessorMX.getStage()==org.apache.coyote.Constants.STAGE_SERVICE;
     }
 }
-//edit at 2019-10-13 01:12:30
+//edit at 2019-10-13 01:20:44
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2019-10-13 01:12:30
+//edit at 2019-10-13 01:20:44
