805a8137823a324c4c4d1b506f800904a7a7a20a
zhaoming <zhaoming@gacrnd.com>
2019-12-23 12:30:27 +0800

    add *.java at 2019-12-23 12:30:25

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello { 
 	public void test() { 
-		// edit at 2019-12-23 12:26:34
+		// edit at 2019-12-23 12:30:25
 	}
 } 
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2019-12-23 12:26:34
+//edit at 2019-12-23 12:30:25
