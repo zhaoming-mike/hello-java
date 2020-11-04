af78d3d77fa23bc5bbe94700036a02b42bf09cbe
zhaoming <zhaoming@gacrnd.com>
2020-11-05 00:23:22 +0800

    add *.java at 2020-11-05 00:23:17

--- Request.java
+++ Request.java
@@ -545,4 +545,4 @@ public final class Request {
         return reqProcessorMX.getStage()==org.apache.coyote.Constants.STAGE_SERVICE;
     }
 }
-//edit at 2020-11-05 00:13:18
+//edit at 2020-11-05 00:23:17
--- Response.java
+++ Response.java
@@ -538,4 +538,4 @@ public final class Response {
         return outputBuffer.getBytesWritten();
     }
 }
-//edit at 2020-11-05 00:13:18
+//edit at 2020-11-05 00:23:17
